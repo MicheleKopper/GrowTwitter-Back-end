0ac04bf9ac464cfb9e19c22e7deed1a13e495b17
Michele Kopper <michelekopper@gmail.com>
2024-11-27 10:11:39 -0300

    Config prisma

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,33 @@
-console.log("Olá, mundo");
+import "dotenv/config";
+import cors from "cors";
+import express from "express";
+
+// Servidor express
+const app = express();
+const porta = process.env.PORTA;
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Rota padrão
+app.get("/", (req, res) => {
+  res.status(200).json({
+    ok: true,
+    message: "Api Prisma 💛",
+  });
+});
+
+// Rota Prisma client
+// app.get("/", async (req, res) => {
+//   const nome_tabela = await repository.nome_tabela.findMany();
+
+//   res.status(200).json({ 
+//     ok: true, 
+//     message: "💛", dado: nome_tabela });
+// });
+
+// Iniciar o servidor
+app.listen(porta, () => {
+  console.log(`Servidor rodando na porta: ${porta} 💛`);
+});
