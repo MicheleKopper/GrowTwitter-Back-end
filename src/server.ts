8e8869eab472d43d754d7cdaf0c6bff05d0500aa
Michele Kopper <michelekopper@gmail.com>
2025-02-21 19:49:00 -0300

    Ajustes na porta para deply no render

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,11 @@
 import "dotenv/config";
 import { createServer } from "./express.server";
 
-const porta = process.env.PORTA;
 
-// Importar do `express.server` que criamos
+const porta = process.env.PORT || 3000; 
+
 const app = createServer();
 
-// Iniciar o servidor
 app.listen(porta, () => {
   console.log(`Servidor rodando na porta: ${porta} 💛`);
 });
